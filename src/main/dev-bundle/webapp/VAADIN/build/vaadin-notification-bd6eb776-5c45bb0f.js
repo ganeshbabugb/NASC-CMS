f5cc01f4ceeb6df91101bc06441404873e99963c
Ganesh babu <ganeshbabugb2525@gmail.com>
2023-12-14 20:16:32 +0530

    Added new MarkView with GridPro

---
+++ src/main/dev-bundle/webapp/VAADIN/build/vaadin-notification-bd6eb776-5c45bb0f.js
@@ -0,0 +1,1 @@
+import{y as a,A as t}from"./indexhtml-8531805e.js";const o={tagName:"vaadin-notification",displayName:"Notification",elements:[{selector:"vaadin-notification-card::part(overlay)",displayName:"Notification card",properties:a},{selector:"vaadin-notification-card::part(content)",displayName:"Content",properties:t}]};export{o as default};
